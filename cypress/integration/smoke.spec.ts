21abf72a7995078ba1f8c5652f9903116f27b921
ncphi <nolan@ncphi.com>
2021-04-04 11:45:28 -0300

    @testing-library/cypress

--- cypress/integration/smoke.spec.ts
+++ cypress/integration/smoke.spec.ts
@@ -2,6 +2,6 @@ describe("Smoke Test", () => {
   it("Visits the Next.js Application", () => {
     cy.visit("http://localhost:3000")
 
-    cy.contains("Welcome to Next.js")
+    cy.findByText("Welcome to Next.js")
   })
 })
